6964e6c795a410b7b306ca050309a03195136d6d
val kharitonashvili <valeri.kharitonashvili1@gmail.com>
2015-11-20 13:23:27 +0400

    input

--- application/views/includes/doc/components/headings.php
+++ application/views/includes/doc/components/headings.php
@@ -11,8 +11,29 @@
 		<h2 class="h2-do">h2 heading</h2>
 		<h3 class="h3-do">h3 heading</h3>
 		<h4 class="h4-do">h4 heading</h4>
+
+
 		<h5 class="h5-do">h5 heading</h5>
 		<h6 class="h6-do">h6 heading</h6>
+		<pre class="pre"><code class="html">
+			&lt;h1 class="h1-do">h1 heading&lt;/h1>
+			<br>
+			&lt;h2 class="h2-do">h2 heading&lt;/h2>
+			<br>
+			&lt;h3 class="h3-do">h3 heading&lt;/h3>
+			<br>
+			&lt;h4 class="h4-do">h4 heading&lt;/h4>
+			<br>
+			&lt;h5 class="h5-do">h5 heading&lt;/h5>
+			<br>
+			&lt;h6 class="h6-do">h6 heading&lt;/h6>
+<!-- 
+			<?php 
+				echo htmlspecialchars('<h1>valeri</h1>');
+			?> -->
+
+		</code></pre>
+
 		<div class="hr-do"></div>
 		<h1 class="h1-do">h1 heading<small class="h1-do-small">some text like this</small></h1>
 		<h2 class="h2-do">h2 heading<small class="h2-do-small">some text like this</small></h2>
